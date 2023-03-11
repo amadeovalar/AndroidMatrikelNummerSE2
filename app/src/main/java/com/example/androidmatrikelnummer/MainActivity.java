6dda7f07b92b8c02cfeace8085ff5ad77712fda2
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2023-03-11 13:14:10 +0100

    Getting the text from the user input

--- app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
+++ app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
@@ -3,27 +3,31 @@ package com.example.androidmatrikelnummer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
-
+    EditText matrikelNummerInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+//        Get the text from the user
+        matrikelNummerInput = findViewById(R.id.MatrikelNummerInput);
+//
 //        Identify the button
-        submitButton.findViewById(R.id.submit_button);
-
+        submitButton = findViewById(R.id.submit_button);
+//
 //        Create a click lister
         submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.v("EditText", matrikelNummerInput.getText().toString());
             }
         });
 
