d7cb7a2e823bdbaadc0b444e32e8f9edd78422da
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2023-03-11 10:38:17 +0100

    Setting up a button click listener

--- app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
+++ app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.androidmatrikelnummer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//        Identify the button
+        submitButton.findViewById(R.id.submit_button);
+
+//        Create a click lister
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
