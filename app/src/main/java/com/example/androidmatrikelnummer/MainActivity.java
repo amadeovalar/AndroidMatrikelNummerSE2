567e451cb91bf5be21acfb92a123e4d47f7649d8
amadeovalar <56834491+amadeovalar@users.noreply.github.com>
2023-03-11 19:17:04 +0100

    Making a new thread, it builds no errors, not showing message though

--- app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
+++ app/src/main/java/com/example/androidmatrikelnummer/MainActivity.java
@@ -7,11 +7,23 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
     Button submitButton;
     EditText matrikelNummerInput;
+    TextView messageFromServer;
+
+    String serverName = "se2-isys.aau.at";
+    Integer portNumber = 53212;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,18 +31,50 @@ public class MainActivity extends AppCompatActivity {
 
 //        Get the text from the user
         matrikelNummerInput = findViewById(R.id.MatrikelNummerInput);
-//
 //        Identify the button
         submitButton = findViewById(R.id.submit_button);
+//        Identify the text message where the server message will be displayed
+        messageFromServer = findViewById(R.id.serverMessage);
+
+
 //
 //        Create a click lister
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.v("EditText", matrikelNummerInput.getText().toString());
-            }
+        submitButton.setOnClickListener(view -> {
+            Log.v("EditText", matrikelNummerInput.getText().toString());
+
         });
 
+    }
+
+    public void onClickSubmit(View view) {
+
+//        Initiate a new thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket = new Socket(serverName, portNumber);
+
+//                    Reading the the server message
+                    BufferedReader bufferedReader =
+                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String serverMessage = bufferedReader.readLine();
+
+//                    returning back to the UI thread from worker thread
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            messageFromServer.setText(serverMessage);
+                        }
+                    });
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
 
     }
+
 }
+
